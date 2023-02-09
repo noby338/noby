d8734e1fd2e6ae7458856d88c7efc94dd94a5f50
noby <1326981297@qq.com>
2023-02-09 17:10:17 +0800

    解决github pages _plugin-vue_export-helper 404 问题

--- src/.vuepress/dist/assets/影音.html.97f2ee5a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c as _}from"./app.0ff00f8b.js";const c={};function o(r,n){return t(),_("div")}const f=e(c,[["render",o],["__file","影音.html.vue"]]);export{f as default};
