be2fda98276d0519aa1bb3245858615b5bd5904b
Noby338 <1326981297@qq.com>
2023-11-30 10:08:11 +0800

    标题修改为编程笔记

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "个人新站",
+      title: "Noby 的编程笔记",
       // 设置favicon
       head: [
         ["link", { rel: "icon", href: "/site_logo.png" }],
